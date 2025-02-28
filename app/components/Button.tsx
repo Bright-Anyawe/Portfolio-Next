505bf603e085a5855efffe1b1bd432ce3c49528b
Bright Anyawe <brightanyawe55@gmail.com>
2025-02-28 18:16:05 +0000

    Change resume

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -7,7 +7,7 @@ export default function HomeButton() {
       <div className="flex flex-col xl:flex-row items-center gap-12">
         {}
         <a
-          href="./Web Developer[Cv].pdf"
+          href="./Anyawe Resume.pdf"
           download
           className="sheen-button justify-center whitespace-nowrap rounded-full font-semibold ring-offset-white transition-colors bg-transparent text-accent border border-accent hover:bg-accent-hover h-[56px] px-8 text-sm tracking-[2px] uppercase flex items-center gap-2"
         >
